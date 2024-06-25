6062ed311fa8c3c2136d297e7e4fe9e04f63589b
ivan <parfenov_ivan_42a@mail.ru>
2024-06-25 11:30:39 +0700

    init

--- nero/nero/management/commands/loadwords.py
+++ nero/nero/management/commands/loadwords.py
@@ -3,4 +3,10 @@ from django.core.management import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open('words.txt', 'r') as f:
+
+        try:
+            lines = open('words.txt').readlines()
+            for line in lines[:10]:
+                print(line)
+        except FileNotFoundError:
+            print('words.txt not found')
