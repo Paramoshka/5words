76d770094d1d644e9be9c8aee62f6776de811b7d
ivan <parfenov_ivan_42a@mail.ru>
2024-06-26 21:01:53 +0700

    add data to tensor

--- nero/nero/data/model.py
+++ nero/nero/data/model.py
@@ -1,7 +1,17 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 #https://www.appsilon.com/post/visualize-pytorch-neural-networks
-class Model(nn.Module):
+class Predictor(nn.Module):
     def __init__(self):
         super().__init__()
+        self.input = nn.Linear(in_features=4, out_features=16)
+        self.hidden_1 = nn.Linear(in_features=16, out_features=16)
+        self.output = nn.Linear(in_features=16, out_features=3)
+
+    def forward(self, x):
+        x = F.relu_(self.input(x))
+        x = F.relu_(self.hidden_1(x))
+        return self.output(x)
+
--- nero/nero/management/commands/train.py
+++ nero/nero/management/commands/train.py
@@ -1,8 +1,10 @@
 from django.core.management import BaseCommand
 from nero.data.data import Data
+from nero.data.model import Predictor
 
 #https://botfactory.in/2023/06/07/building-a-simple-rnn-neural-network-with-pytorch-a-step-by-step-guide/
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Training neuron", ending="")
         model = Data()
+        predictor = Predictor()
