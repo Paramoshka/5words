c075da33d07d7accdf9d12447e4fe0eddc2ab503
ivan <parfenov_ivan_42a@mail.ru>
2024-07-02 20:01:45 +0700

    add data to tensor

--- nero/nero/management/commands/predict.py
+++ nero/nero/management/commands/predict.py
@@ -1,5 +1,7 @@
 import random
 import string
+
+import torch
 from django.core.management.base import BaseCommand
 from nero.predict.model import RNN
 from nero.predict.data import Data
@@ -7,6 +9,8 @@ from nero.predict.train import train
 
 all_letters = string.ascii_letters + " .,;'"
 n_letters = len(all_letters)
+
+
 class Command(BaseCommand):
     data = Data()
     n_categories = len(data.all_categories)
@@ -15,9 +19,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Predict'))
-        category = self.data.all_categories[random.randint(0, self.n_categories - 1)]
-        line = self.data.category_lines[category]
-        train(self.rnn, category, line, )
-       # train(self.rnn, )
+        category_name = self.data.all_categories[random.randint(0, self.n_categories - 1)]
+        line = self.data.category_lines[category_name]
+        #train(self.rnn, category_name, line, )
+        self.category_tensor(category_name)
 
-    def category_tensor(self):
+    def category_tensor(self, category_name) -> torch.Tensor:
+        li = self.data.all_categories.index(category_name)
+        tensor = torch.zeros(1, len(self.data.all_categories))
+        tensor[0][li] = 1
+        return tensor
